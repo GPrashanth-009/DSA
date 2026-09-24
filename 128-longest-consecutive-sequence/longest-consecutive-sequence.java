ecb7228216e446c1dc0680b3acd7b5e5f6f992cd
GPrashanth-009 <givilikapallyprashanthca@gmail.com>
2026-09-24 18:47:53 +0530

    Time: 21 ms (96.01%) | Memory: 77.3 MB (94.50%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,74 +1,70 @@
-// class Solution {
-//     public int longestConsecutive(int[] nums) {
-//         int n=nums.length;
-//         if (n == 0) {
-//             return 0;
-//         }
-//         // Selection sort algorithm:
-
-//         // for (int i = 0; i < n - 1; i++) {
-//         // int minIndex = i;
-
-//         //     for (int j = i + 1; j < n; j++) {
-//         //         if (nums[j] < nums[minIndex]) {
-//         //             minIndex = j;
-//         //         }
-//         //     }
-
-//         //     int temp = nums[i];
-//         //     nums[i] = nums[minIndex];
-//         //     nums[minIndex] = temp;
-//         // }
-//         Arrays.sort(nums);
-//         int c=1;
-//         int m=1;
-//         for(int i=0;i<n-1;i++){
-//             if((nums[i])!=nums[i+1]){
-          
-//                 if((nums[i]+1)==nums[i+1]){
-//                     c++;
-//                 }
-//                 else{
-//                     m=Math.max(m,c);
-//                     c=1;
-//                 }
-//             }
-//         }
-//         return Math.max(m,c);
-        
-//     }
-// }
-
-
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
+        int n=nums.length;
+        if (n == 0) {
+            return 0;
         }
+        // Selection sort algorithm:
 
-        int maxStreak = 0;
+        // for (int i = 0; i < n - 1; i++) {
+        // int minIndex = i;
 
-        for (int num : set) {
-            // Check if this number is the absolute start of a sequence
-            if (!set.contains(num - 1)) {
-                int currentNum = num;
-                int currentStreak = 1;
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (nums[j] < nums[minIndex]) {
+        //             minIndex = j;
+        //         }
+        //     }
 
-                // Track the continuous sequence forward
-                while (set.contains(currentNum + 1)) {
-                    currentNum += 1;
-                    currentStreak += 1;
+        //     int temp = nums[i];
+        //     nums[i] = nums[minIndex];
+        //     nums[minIndex] = temp;
+        // }
+        Arrays.sort(nums);
+        int c=1;
+        int m=1;
+        for(int i=0;i<n-1;i++){
+            if((nums[i])!=nums[i+1]){
+          
+                if((nums[i]+1)==nums[i+1]){
+                    c++;
                 }
-
-                maxStreak = Math.max(maxStreak, currentStreak);
+                else{
+                    m=Math.max(m,c);
+                    c=1;
+                }
             }
         }
-        return maxStreak;
+        return Math.max(m,c);
+        
     }
 }
+
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         if (nums == null || nums.length == 0) return 0;
+
+//         Set<Integer> set = new HashSet<>();
+//         for (int num : nums) {
+//             set.add(num);
+//         }
+
+//         int maxStreak = 0;
+
+//         for (int num : set) {
+//             // Check if this number is the absolute start of a sequence
+//             if (!set.contains(num - 1)) {
+//                 int currentNum = num;
+//                 int currentStreak = 1;
+
+//                 // Track the continuous sequence forward
+//                 while (set.contains(currentNum + 1)) {
+//                     currentNum += 1;
+//                     currentStreak += 1;
+//                 }
+
+//                 maxStreak = Math.max(maxStreak, currentStreak);
+//             }
+//         }
+//         return maxStreak;
+//     }
+// }
