67f2c54016c9859e958252e03359f7cc1d663ff5
GPrashanth-009 <givilikapallyprashanthca@gmail.com>
2026-09-24 18:46:30 +0530

    Time: 32 ms (36.81%) | Memory: 95.7 MB (27.22%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,39 +1,74 @@
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         int n=nums.length;
+//         if (n == 0) {
+//             return 0;
+//         }
+//         // Selection sort algorithm:
+
+//         // for (int i = 0; i < n - 1; i++) {
+//         // int minIndex = i;
+
+//         //     for (int j = i + 1; j < n; j++) {
+//         //         if (nums[j] < nums[minIndex]) {
+//         //             minIndex = j;
+//         //         }
+//         //     }
+
+//         //     int temp = nums[i];
+//         //     nums[i] = nums[minIndex];
+//         //     nums[minIndex] = temp;
+//         // }
+//         Arrays.sort(nums);
+//         int c=1;
+//         int m=1;
+//         for(int i=0;i<n-1;i++){
+//             if((nums[i])!=nums[i+1]){
+          
+//                 if((nums[i]+1)==nums[i+1]){
+//                     c++;
+//                 }
+//                 else{
+//                     m=Math.max(m,c);
+//                     c=1;
+//                 }
+//             }
+//         }
+//         return Math.max(m,c);
+        
+//     }
+// }
+
+
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int n=nums.length;
-        if (n == 0) {
-            return 0;
+        if (nums == null || nums.length == 0) return 0;
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
-        // for (int i = 0; i < n - 1; i++) {
-        // int minIndex = i;
 
-        //     for (int j = i + 1; j < n; j++) {
-        //         if (nums[j] < nums[minIndex]) {
-        //             minIndex = j;
-        //         }
-        //     }
+        int maxStreak = 0;
 
-        //     int temp = nums[i];
-        //     nums[i] = nums[minIndex];
-        //     nums[minIndex] = temp;
-        // }
-        Arrays.sort(nums);
-        int c=1;
-        int m=1;
-        for(int i=0;i<n-1;i++){
-            if((nums[i])!=nums[i+1]){
-          
-                if((nums[i]+1)==nums[i+1]){
-                    c++;
+        for (int num : set) {
+            // Check if this number is the absolute start of a sequence
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                // Track the continuous sequence forward
+                while (set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
                 }
-                else{
-                    m=Math.max(m,c);
-                    c=1;
-                }
+
+                maxStreak = Math.max(maxStreak, currentStreak);
             }
         }
-        return Math.max(m,c);
-        
+        return maxStreak;
     }
 }
-
