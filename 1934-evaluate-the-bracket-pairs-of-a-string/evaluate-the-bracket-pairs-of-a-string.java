beb3c330734bb20cefe0bf7989fbb68c45d97360
GPrashanth-009 <givilikapallyprashanthca@gmail.com>
2026-09-26 17:51:38 +0530

    Time: 40 ms (42.99%) | Memory: 90.5 MB (88.23%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        Map<String, String> map = new HashMap<>();
+        for (List<String> pair : knowledge) {
+            map.put(pair.get(0), pair.get(1));
+        }
+
+        StringBuilder result = new StringBuilder();
+        StringBuilder key = new StringBuilder();
+        boolean inBracket = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                inBracket = true;
+            } else if (ch == ')') {
+                inBracket = false;
+                result.append(map.getOrDefault(key.toString(), "?"));
+                key.setLength(0);
+            } else {
+                if (inBracket) {
+                    key.append(ch);
+                } else {
+                    result.append(ch);
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
